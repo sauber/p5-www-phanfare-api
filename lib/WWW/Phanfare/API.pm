911737b9964b4cbb959fc3011924f752e932d916
Soren Dossing <netcom@sauber.net>
2010-12-03 17:24:14 +0900

    A much simpler approach to cover all current and future methods by using autoload and no check of required parameters

--- lib/WWW/Phanfare/API.pm
+++ lib/WWW/Phanfare/API.pm
@@ -1,15 +1,5 @@
 package WWW::Phanfare::API;
 
-use Moose;
-use MooseX::Method::Signatures;
-use REST::Client;
-use Digest::MD5 qw(md5_hex);
-use Data::Dumper;
-use XML::Simple;
-use Config::General;
-use File::HomeDir;
-use Carp;
-
 =head1 NAME
 
 WWW::Phanfare::API - Perl wrapper for Phanfare API
@@ -20,227 +10,120 @@ Version 0.01
 
 =cut
 
+use strict;
+use warnings;
+use Carp;
+use REST::Client;
+use Digest::MD5 qw(md5_hex);
+use URI::Escape;
+
 our $VERSION = '0.01';
+our $site = 'http://www.phanfare.com/api/?';
+our $AUTOLOAD;
+
+sub new {
+  my $that  = shift;
+  my $class = ref($that) || $that;
+  my $self  = { @_ };
+  bless $self, $class;
+  return $self;
+}
+
+# All API methods implemented as autoload functions.
+#
+#   $papi->Function() becomes REST::Client::GET(..."method=Function"...)
+#
+sub AUTOLOAD {
+  my $self = shift;
+  croak "$self is not an object" unless ref($self);
+
+  my $method = $AUTOLOAD;
+  $method =~ s/.*://;   # strip fully-qualified portion
+  croak "method not defined" unless $method;
+
+  # Verify keys are defined
+  croak 'api_key not defined' unless $self->{api_key};
+  croak 'private_key not defined' unless $self->{private_key};
+
+  my %param = @_;
+  # Build signature request string
+  my $req = join '&',
+    sprintf('%s=%s', 'api_key', $self->{api_key}),
+    sprintf('%s=%s', 'method', $method),
+    map { sprintf '%s=%s', $_, $param{$_} } keys %param;
+
+  # Sign request string
+  my $sig = md5_hex( $req . $self->{private_key} );
+
+  # Build URL escaped request string
+  $req = join '&',
+    sprintf('%s=%s', 'api_key', $self->{api_key}),
+    sprintf('%s=%s', 'method', $method),
+    map { sprintf '%s=%s', $_, uri_escape $param{$_} } keys %param;
+  $req .= sprintf '&%s=%s', 'sig', $sig;
+
+  #warn "*** Request string: $req\n"; # XXX debug
+
+  # Create REST agent with cookies
+  unless ( $self->{_rest} ) {
+    $self->{_rest} = new REST::Client;
+    $self->{_rest}->getUseragent()->cookie_jar({});
+  }
+
+  # Send request
+  $self->{_rest}->GET( $site . $req );
+
+  # Receive response
+  carp sprintf(
+    "Return code %s: %s",
+    $self->{_rest}->responseCode(),
+    $self->{_rest}->responseContent()
+  ) unless $self->{_rest}->responseCode() eq '200';
+
+  # Return the XML formatted response
+  return $self->{_rest}->responseContent();
+}
+
+# Make sure not caught by AUTOLOAD
+sub DESTROY {}
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
+Create object. Developer API keys required.
 
     use WWW::Phanfare::API;
+    my $papi = WWW::Phanfare::API->new(
+      api_key     => 'xxx',
+      private_key => 'yyy',
+    );
 
-    my $phanfare = WWW::Phanfare::API->new();
-    ...
+Authentication with account:
 
-=head1 EXPORT
+    $papi->Authenticate(
+       email_address => 'my@email',
+       password      => 'zzz',
+    )
+ 
+Or authenticate as guest:
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+    $papi->AuthenticateGuest();
 
-=cut
+=head1 DESCRIPTION
 
-has _xml => (
-  is => 'ro',
-  isa => 'XML::Simple',
-  lazy_build => 1,
-);
-sub _build__xml { new XML::Simple }
-
-has _rest => (
-  is => 'ro',
-  isa => 'REST::Client',
-  required => 1,
-  lazy_build => 1,
-);
-sub _build__rest {
-  # Create a new REST client with cookies enabled
-  my $ua = new REST::Client;
-  $ua->getUseragent()->cookie_jar({});
-  return $ua;
-}
-
-# User Configs
-has email_address => ( is => 'ro', isa => 'Str', lazy_build=>1 );
-sub _build_email_address {
-  _readconfig('email_address');
-}
-
-has password => ( is => 'ro', isa => 'Str', lazy_build=>1 );
-sub _build_password {
-  _readconfig('password');
-}
-
-has api_key => ( is => 'ro', isa => 'Str', required => 1, lazy_build => 1 );
-sub _build_api_key {
-  _readconfig('api_key');
-}
-
-has private_key => ( is => 'ro', isa => 'Str', required => 1, lazy_build=>1 );
-sub _build_private_key {
-  _readconfig('private_key');
-}
-
-has _site => ( is => 'ro', isa => 'Str', default => 'http://www.phanfare.com/api/?' );
-has target_uid => ( is => 'rw', isa => 'Str' );
-has _requeststring => ( is => 'rw', isa => 'Str', default=>'', clearer => '_clear__requeststring' );
-
-has _authenticated => ( is => 'rw', isa => 'Bool' );
+Low level implementation of the Phanfare API. A developer API key is required
+for using this module.
 
 =head1 SUBROUTINES/METHODS
 
-=head2 Private functions
+Refer to methods and required parameters are listed on
+http://help.phanfare.com/index.php/API . api_key and private_code is only
+required for the constructor and not for individual methods.
 
-=cut
+Methods return an unprocessed xml string from REST GET call.
 
-# Read configuration options from .phanfarerc file
-#
-sub _readconfig {
-  my $option = shift;
-  # Make sure file exists
-  my $rcfile = File::HomeDir->my_home . "/.phanfarerc";
-  unless ( -r $rcfile ) {
-    croak "Cannot read $rcfile\n";
-  }
-  
-  # Read config file
-  my $conf = new Config::General($rcfile);
-  my %config = $conf->getall;
-  
-  # Read each required value
-  #for my $key ( qw(email password api_key private_key) ) {
-  #  croak "$key not defined in $rcfile" unless $config{$key};
-  #  $self->$key( $config{$key} );
-  #}
+=head2 new
 
-  return $config{$option};
-}
-
-# Sign a request with private key
-#
-method _sign {
-  my $req = $self->_requeststring;
-  my $sig = md5_hex( $req . $self->private_key );
-  $self->_append( 'sig' => $sig );
-  return $self;
-}
-
-#Append key-value pairs to request string
-#
-method _append( Str $key, Str $value ) {
-  my $str;
-  my $req = $self->_requeststring;
-  $str = '&' if $req;
-  $req ||= ''; $str ||= '';
-  $str = $req . $str . sprintf "%s=%s", $key, $value;  
-  $self->_requeststring( $str );
-  return $self;
-}
-
-
-#Place a request to REST API
-#
-method _request {
-  # Place request
-  my $rest = $self->_rest;
-  $rest->GET( $self->_site . $self->_requeststring);
-  $self->_clear__requeststring;
-
-  # Verify request is succesful
-  croak $rest->responseContent() unless $rest->responseCode() eq '200';
-
-  # Convert xml data to perl data structure
-  my $data = $self->_xml->XMLin( $rest->responseContent() );
-  return $data;
-}
-
-########################################################################
-### Authentication
-########################################################################
-
-=head2 Authentication
-
-There are three methods of authentication.
-
-=over
-
-=item Authenticate - read-write account session
-
-=item AuthenticateGuest - read-only session
-
-=item AuthenticateToSite - read-only session to password protected site
-
-=back
-
-One of these methods must be called before any other method can be called.
-
-=cut
-
-# Call an authentication method.
-#
-method _authentication {
-  # If email/password exist, then get read-write session
-  # Otherwise guest session
-  if ( $self->email_address and $self->password ) {
-    $self->Authenticate;
-  } else {
-    $self->AuthenticateGuest;
-  }
-}
-
-=head3 Authenticate
-
-Read-write user session authentication request.
-
-=cut
-
-method Authenticate {
-  $self->_append( 'api_key'  => $self->api_key );
-  $self->_append( 'email_address'    => $self->email_address );
-  $self->_append( 'password' => $self->password );
-  $self->_append( 'method'   => 'Authenticate' );
-  my $resp = $self->_sign->_request;
-  # Read uid from response
-  #warn Dumper $resp;
-  $self->target_uid( $resp->{session}{uid} );
-  $self->_authenticated(1);
-  return $self;
-}
-
-=head3 AuthenticateGuest
-
-Anonymous read-only authentication request
-
-=cut
-
-method AuthenticateGuest {
-  $self->_append( 'api_key'  => $self->api_key );
-  $self->_append( 'method'   => 'AuthenticateGuest' );
-  $self->_sign->_request;
-  $self->_authenticated(1);
-  return $self;
-}
-
-########################################################################
-### Albums
-########################################################################
-
-=head2 Albums
-
-=head3 GetAlbumList
-
-List of albums
-
-=cut
-
-method GetAlbumList {
-  $self->_authentication unless $self->_authenticated;
-  $self->rest->_append( 'api_key'  => $self->api_key );
-  $self->rest->_append( 'method'   => 'GetAlbumList' );
-  my $resp = $self->_sign->_request;
-  # Read list of albums
-  return $resp->{albums}{album};
-}
+Create a new API agent.
 
 =head1 AUTHOR
 
@@ -298,9 +181,6 @@ by the Free Software Foundation; or the Artistic License.
 
 See http://dev.perl.org/licenses/ for more information.
 
-
 =cut
 
-no Moose;
-
-1; # End of WWW::Phanfare::API
+1;
--- t/authenticate.t
+++ t/authenticate.t
@@ -1,8 +1,94 @@
+# If a .phanfarerc file exist, use it for authentication test
+# File format:
+#   api_key xxx
+#   private_key yyy
+#   email_address my@email
+#   password zzz
+#
+
+
 use Test::More qw(no_plan);
+
+eval 'use File::HomeDir';
+plan skip_all => 'File::HomeDir required' if $@;
+
+eval 'use Config::General';
+plan skip_all => 'Config::General required' if $@;
+
+my $rcfile = File::HomeDir->my_home . "/.phanfarerc";
+my $conf;
+SKIP: {
+  skip 'Cannot read $rcfile', 1, unless -r $rcfile;
+
+  $conf = new_ok( Config::General => [ $rcfile ] );
+}
+exit unless $conf;
+
+
+my %config = $conf->getall;
+ok( $config{api_key}, 'api_key not found in .phanfarerc file' );
+ok( $config{private_key}, 'private_key not found in .phanfarerc file' );
+plan skip_all => 'Cannot test authentiction without api_key and private_key'
+  unless $config{api_key} and $config{private_key};
+
 BEGIN { use_ok "WWW::Phanfare::API"};
 require_ok( 'WWW::Phanfare::API' );
+my $api = new WWW::Phanfare::API->new(
+  api_key     => $config{api_key},
+  private_key => $config{private_key},
+);
+ok ( $api, 'Could not create WWW::Phanfare::API object' );
+plan skip_all => 'Could not create WWW::Phanfare::API object' unless $api;
+#diag( $api );
 
-my $api = new WWW::Phanfare::API;
-$api->_authentication;
-#diag("Target uid is " . $api->target_uid);
-ok( $api->target_uid > 0, "Target uid is " . $api->target_uid );
+# Make XML::Simple object
+#
+my $xml;
+eval 'use XML::Simple';
+if ( $@ ) {
+  fail('XML::Simple required');
+} else {
+  $xml = new XML::Simple;
+  ok ( $xml, 'xml object' );
+}
+
+# Test anonymous login
+#
+my $anon = $api->AuthenticateGuest();
+ok( $anon );
+
+# Check for valid response
+# 
+SKIP: {
+  skip 'XML::Simple required', 2, unless $xml and $anon;
+
+  my $ref = $xml->XMLin( $anon );
+  ok( ref $ref, 'XML response not parsing');
+  ok( $ref->{'stat'} eq 'ok', "Response stat not ok: $anon" );
+}
+
+# Test user login when email_address and password is defined in .phanfarerc
+#
+my $user;
+SKIP: {
+  skip 'Cannot test authentiction without email_address and password', 1,
+    unless $config{email_address} and $config{password};
+
+  $user = $api->Authenticate(
+    email_address => $config{email_address},
+    password      => $config{password},
+  );
+
+  ok ( $user );
+};
+
+# Check for valid response
+#
+SKIP: {
+  skip 'XML::Simple required', 2, unless $xml and $user;
+
+  $ref = $xml->XMLin( $user );
+  ok( ref $ref, 'XML response not parsing');
+  ok( $ref->{'stat'} eq 'ok', "Response stat not ok: $user" );
+  ok( $ref->{session}{uid} > 0, 'No target_uid in response' );
+};
