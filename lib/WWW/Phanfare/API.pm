a3b05845af08c6959d82225eaec28d4bdc9198c2
Soren Dossing <netcom@sauber.net>
2010-12-04 15:36:27 +0900

    Correct version number

--- lib/WWW/Phanfare/API.pm
+++ lib/WWW/Phanfare/API.pm
@@ -6,7 +6,7 @@ WWW::Phanfare::API - Perl wrapper for Phanfare API
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
@@ -18,7 +18,7 @@ use Digest::MD5 qw(md5_hex);
 use URI::Escape;
 use XML::Simple;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 our $site = 'http://www.phanfare.com/api/?';
 our $AUTOLOAD;
 
